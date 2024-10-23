2e278623386db64c4712f21cfdab2bbb4b4eb089
DevRafaelLuz <rafael.luz2005@gmail.com>
2024-10-23 15:23:38 -0300

    Página principal e sistema de login desenvolvidos

---
+++ Page/login.js
@@ -0,0 +1,46 @@
+const btnLogin = document.getElementById('btnLogin');
+const btnFechar = document.getElementById('btnFechar');
+
+btnLogin.onclick = function() {
+    login.showModal();
+}
+
+btnFechar.onclick = function() {
+    login.close();
+}
+
+const login = document.getElementById('login');
+const formLogin = document.querySelector('#login form');
+
+let dadosUsuario = [
+    { nome: "teste", email: "teste@teste.com", senha: 13579 },
+];
+
+formLogin.addEventListener('submit', evento => {
+    evento.preventDefault();
+
+    let msgErro = document.querySelector('.erro');
+    if (msgErro) login.removeChild(msgErro);
+
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+
+    dadosUsuario.forEach(usuario => {
+        if (email == usuario.email && senha == usuario.senha) {
+            sessionStorage.setItem('usuarioLogado', true);
+            sessionStorage.setItem('nomeUsuario', usuario.nome);
+
+            window.location.href = "../Admin/index.html";
+        }
+    });
+
+    let usuarioLogado = sessionStorage.getItem('usuarioLogado');
+
+    if (!usuarioLogado) {
+        let erro = document.createElement('p');
+        erro.classList.add("erro");
+        erro.innerText = "Login ou senha inválidos!";
+        login.insertBefore(erro, login.firstChild);
+        document.querySelector("#login form").reset();
+    }
+});
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Este projeto visa criar um site de tema livre, usando HTML, CSS E JavaScript.
 * Formulários de Cadastro
 * Páginas de Relatórios
 * Páginas de Conteúdo
+
+## 🔗Wireframe do Projeto
+> https://www.figma.com/design/onPLIIStoRrBzc4RKiL9wc/WireframeProjetoWeb?node-id=0-1&t=a2qt9XIL2VE6LryK-1
